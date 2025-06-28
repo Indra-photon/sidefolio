8ec780a359987fa798ed43bf1a49a33f7e87e905
Indranil Maiti <indranilmaiti1@gmail.com>
2025-06-28 10:27:38 +0200

    deploy-fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   return (
     <div className="p-4 text-center justify-center text-xs text-neutral-500 border-t border-neutral-100">
       <span className="font-semibold">{new Date().getFullYear()} </span>
-      &#8212; Let's build something amazing together!
+      &#8212; Let us build something amazing together!
     </div>
   );
 };
